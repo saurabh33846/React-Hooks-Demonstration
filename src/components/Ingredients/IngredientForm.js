1f4d74cee187a4f9839ea2e1775529817e1fa725
saurabh kumar <saurabh.kumar07@sap.com>
2020-05-02 13:46:34 +0530

    Initial commit

---
+++ src/components/Ingredients/IngredientForm.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+import Card from '../UI/Card';
+import './IngredientForm.css';
+
+const IngredientForm = React.memo(props => {
+  const submitHandler = event => {
+    event.preventDefault();
+    // ...
+  };
+  const inputState = useState({title:'',amount:''});
+  return (
+    <section className="ingredient-form">
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input type="text" id="title" value={inputState[0].title} onChange={
+              (event)=>{
+              inputState[1]({title:event.target.value})
+              console.log(event.target.value);}
+              }/>
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input type="number" id="amount"  value={inputState[0].amount} onChange={event=>inputState[1]({amount:event.target.amount})}/>
+          </div>
+          <div className="ingredient-form__actions">
+            <button type="submit">Add Ingredient</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+});
+
+export default IngredientForm;
