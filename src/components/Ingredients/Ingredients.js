c73736c706d788db5282da5ea11b9839b54e058c
saurabh kumar <saurabh.kumar07@sap.com>
2020-05-02 23:14:47 +0530

    added http post to post data to server

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React,{useState}from 'react';
+import React,{useState, useEffect}from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,7 +6,19 @@ import IngredientList from './IngredientList';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+  useEffect();
   const addIngredientHandler = (ingredient) =>{
+    fetch('https://react-hooks-example-19ae6.firebaseio.com/ingredeint.json',{
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        
+      },
+      body: JSON.stringify(ingredient)
+    }).then((response)=>{
+      console.log(response);
+    })
     setUserIngredients((prevIngredients)=>{
       return [...prevIngredients, {...ingredient, id:Math.random().toString()}]
     })
