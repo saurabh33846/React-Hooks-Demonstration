22cc58f3502de12d40cad2b9f54f1220e55acebf
saurabh kumar <saurabh.kumar07@sap.com>
2020-05-02 15:01:36 +0530

    Added first support of useState in function
    Added useState  hook.  Also restructured the code to be compliant with SyntheticEvent provided by react.

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -17,13 +17,22 @@ const IngredientForm = React.memo(props => {
             <label htmlFor="title">Name</label>
             <input type="text" id="title" value={inputState[0].title} onChange={
               (event)=>{
-              inputState[1]({title:event.target.value})
-              console.log(event.target.value);}
+                const sTitle = event.target.value;
+              inputState[1]((prevState)=>{
+                return {title:sTitle, amount:prevState.amount}})
+                console.log(event.target.value);}
               }/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount"  value={inputState[0].amount} onChange={event=>inputState[1]({amount:event.target.amount})}/>
+            <input type="number" id="amount"  value={inputState[0].amount} onChange={
+              (event)=>{
+                const sAmount = event.target.value;
+                inputState[1](
+                 (prevState)=>(
+                   {amount:sAmount,title:prevState.title}
+                   )
+                )}}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
