475d25319e04ab6ca5befc11fd25d90ac510804a
saurabh kumar <saurabh.kumar07@sap.com>
2020-05-02 15:26:12 +0530

    Seperate one state object into multiple states
    It's good to seperate a state into multiple states when using hooks becuase in hooks states can be anyrthing like object array or anything. Thus React does not automatically merge states for us. We can forget to put one state value while updating other. Thus it's good to have seperate states.

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -8,30 +8,31 @@ const IngredientForm = React.memo(props => {
     event.preventDefault();
     // ...
   };
-  const [inputState, setInputState] = useState({title:'',amount:''});
+  // const [inputState, setInputState] = useState({title:'',amount:''});
+  const [enteredTitle, setTitleState] = useState('');
+  const [enteredAmount, setAmountState] = useState('');
   return (
     <section className="ingredient-form">
       <Card>
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState.title} onChange={
+            <input type="text" id="title" value={enteredTitle} onChange={
               (event)=>{
                 const sTitle = event.target.value;
-                setInputState((prevState)=>{
-                return {title:sTitle, amount:prevState.amount}})
+                setTitleState((prevState)=>{
+                return sTitle})
                 console.log(event.target.value);}
               }/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount"  value={inputState.amount} onChange={
+            <input type="number" id="amount"  value={enteredAmount} onChange={
               (event)=>{
                 const sAmount = event.target.value;
-                setInputState(
-                 (prevState)=>(
-                   {amount:sAmount,title:prevState.title}
-                   )
+                setAmountState(
+                 (prevState)=>
+                 sAmount
                 )}}/>
           </div>
           <div className="ingredient-form__actions">
