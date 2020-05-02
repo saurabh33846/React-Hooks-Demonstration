84a2320b543ea312365c93bdb478d12fe7c43426
saurabh kumar <saurabh.kumar07@sap.com>
2020-05-02 15:16:23 +0530

    Array destructuring added for input states for better readability

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -8,27 +8,27 @@ const IngredientForm = React.memo(props => {
     event.preventDefault();
     // ...
   };
-  const inputState = useState({title:'',amount:''});
+  const [inputState, setInputState] = useState({title:'',amount:''});
   return (
     <section className="ingredient-form">
       <Card>
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={
+            <input type="text" id="title" value={inputState.title} onChange={
               (event)=>{
                 const sTitle = event.target.value;
-              inputState[1]((prevState)=>{
+                setInputState((prevState)=>{
                 return {title:sTitle, amount:prevState.amount}})
                 console.log(event.target.value);}
               }/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount"  value={inputState[0].amount} onChange={
+            <input type="number" id="amount"  value={inputState.amount} onChange={
               (event)=>{
                 const sAmount = event.target.value;
-                inputState[1](
+                setInputState(
                  (prevState)=>(
                    {amount:sAmount,title:prevState.title}
                    )
