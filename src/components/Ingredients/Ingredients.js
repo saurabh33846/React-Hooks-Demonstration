09b8f1cf6e02fb02d37e2d64d148a23c8b4d28a0
saurabh kumar <saurabh.kumar07@sap.com>
2020-05-03 10:44:50 +0530

    Added busy state and Error handling

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,9 +3,12 @@ import React,{useState, useEffect, useCallback}from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   // useEffect(()=>{
   //   fetch('https://react-hooks-example-19ae6.firebaseio.com/ingredeint.json').
@@ -28,6 +31,7 @@ function Ingredients() {
     setUserIngredients(filteredIngredients)
   },[]);
   const addIngredientHandler = (ingredient) =>{
+    setIsLoading(true);
     fetch('https://react-hooks-example-19ae6.firebaseio.com/ingredeint.json',{
       method: 'POST', 
       headers: {
@@ -38,36 +42,52 @@ function Ingredients() {
       body: JSON.stringify(ingredient)
     }).then((response)=>{
       console.log(response);
+      setUserIngredients((prevIngredients)=>{
+        setIsLoading(false);
+        return [...prevIngredients, {...ingredient, id:Math.random().toString()}]
+      })
+    }).catch((err)=>{
+      setError('Something went wrong');
     })
-    setUserIngredients((prevIngredients)=>{
-      return [...prevIngredients, {...ingredient, id:Math.random().toString()}]
-    })
+
   }
   const removeIngredientHanlder = (ingredientId)=>{
+    setIsLoading(true);
     fetch(`https://react-hooks-example-19ae6.firebaseio.com/ingredeint/${ingredientId}.json`,{
       method: 'DELETE', 
       headers: {
         'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
-        
       }
     }).then(()=>{
+      setIsLoading(false);
       setUserIngredients((prevIngredients)=>{
         return prevIngredients.filter((pervIng)=>{
           return ingredientId!== pervIng.id
         })
       })
+    }).catch((err)=>{
+      setError('Something went wrong');
     })
+
+  }
+  const clearError = ()=>{
+    /** React batches sync setState, means if below we are setting 2 stataes sync then react 
+     * will group them together and update state and only 1 rerender cycle will happe not 2.
+     */
+    setError(null);
+    setIsLoading(false);
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient = {addIngredientHandler}/>
+      {error? <ErrorModal onClose = {clearError}>{error}</ErrorModal>:null}
+      <IngredientForm onAddIngredient = {addIngredientHandler} loading = {isLoading}/>
 
       <section>
         <Search onLoadingIngredients ={filteredIngredientHandler} />
         {/* Need to add list here! */}
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHanlder}></IngredientList>
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHanlder} ></IngredientList>
       </section>
     </div>
   );
