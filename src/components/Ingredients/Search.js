0a763281d57d904bfd23c61c73d12eac6423740e
saurabh kumar <saurabh.kumar07@sap.com>
2020-05-03 09:52:14 +0530

    Added filter with debuncing using useeffedt returnn function and cleanup

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,35 +1,44 @@
-import React, {useState, useEffect}from 'react';
+import React, {useState, useEffect, useRef}from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
   const {onLoadingIngredients} = props;
   useEffect(()=>{
-    const query = enteredFilter.length=== 0? '':`?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hooks-example-19ae6.firebaseio.com/ingredeint.json'+query).
-    then((response)=>{
-      return response.json()
-    }).
-    then((jsonData)=>{
-      const aIngredeints = [];
-      for(const key in jsonData){
-        aIngredeints.push({
-          id:key,
-          title:jsonData[key].title,
-          amount:jsonData[key].amount
+    const timer = setTimeout(()=>{
+      if(enteredFilter === inputRef.current.value) {
+        const query = enteredFilter.length=== 0? '':`?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-example-19ae6.firebaseio.com/ingredeint.json'+query).
+        then((response)=>{
+          return response.json()
+        }).
+        then((jsonData)=>{
+          const aIngredeints = [];
+          for(const key in jsonData){
+            aIngredeints.push({
+              id:key,
+              title:jsonData[key].title,
+              amount:jsonData[key].amount
+            })
+          }
+          props.onLoadingIngredients(aIngredeints);
         })
       }
-      props.onLoadingIngredients(aIngredeints);
-    })
-  },[enteredFilter, onLoadingIngredients])
+    }, 500)
+    return ()=>{
+      clearTimeout(timer)
+    }
+
+  },[enteredFilter, onLoadingIngredients, inputRef])
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilter} onChange={
+          <input type="text" value={enteredFilter} ref={inputRef} onChange={
             (event)=>{
             setEnteredFilter(event.target.value)}}/>
         </div>
